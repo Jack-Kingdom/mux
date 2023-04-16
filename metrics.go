331b7995acd698933f11f0d82de0e91f299add72
Jack King <email@qiaohong.org>
2023-04-17 01:00:33 +0800

    chore: add objective config for lifetime metrics

--- metrics.go
+++ metrics.go
@@ -7,6 +7,7 @@ import (
 
 var (
 	preciseBuckets = []float64{0.001, .005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5}
+	preciseObjectives = map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001}
 )
 
 var (
@@ -25,10 +26,12 @@ var (
 	sessionLifetimeDurationSummary = promauto.NewSummary(prometheus.SummaryOpts{
 		Name: "SessionLifetimeDurationSummary",
 		Help: "session 存活时间",
+		Objectives: preciseObjectives,
 	})
 
 	streamLifetimeDurationSummary = promauto.NewSummary(prometheus.SummaryOpts{
 		Name: "StreamLifetimeDurationSummary",
 		Help: "stream 持续时间",
+		Objectives: preciseObjectives,
 	})
 )
